6137daed704f21b34fde8c905e88caef1742f8f8
Alexander Semenov <SemenovDev@yahoo.com>
2022-03-24 15:02:53 +0600

    Add content pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import AboutBtn from './Components/NavBar/AboutBtn';
 import ProjectsBtn from './Components/NavBar/ProjectsBtn';
 import SkillBtn from './Components/NavBar/SkillBtn';
 import ContactsBtn from './Components/NavBar/ContactsBtn';
+import AboutMe from './Components/AboutMe';
+import Projects from './Components/Projects';
+import Skills from './Components/Skills';
+import Contacts from './Components/Contacts';
 
 function App() {
   const [content, setContent] = useState('about');
@@ -34,7 +38,17 @@ function App() {
       <ProjectsBtn onClick={() => changeContent('projects')}/>
       <SkillBtn onClick={() => changeContent('skills')}/>
       <ContactsBtn onClick={() => changeContent('contacts')}/>      
-      <div id='content'>{content}</div>
+      <div id='content'>{
+        content === "about"
+          ? <AboutMe />
+          : content === "projects"
+            ? <Projects />
+            : content === "skills"
+              ? <Skills />
+              : content === "contacts"
+                ? <Contacts />
+                : null
+      }</div>
     </div>
   );
 }
--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -2,13 +2,9 @@ import React from "react";
 
 function AboutMe () {
   return (
-    <div>Lorem Ipsum
+    <div>About Me
     "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-    "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."
-    
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel ligula pulvinar, viverra eros at, interdum metus. Etiam a porttitor erat. Donec sit amet sapien sed justo placerat vestibulum. Nam ut varius metus, a aliquet ipsum. Nunc mauris enim, scelerisque sit amet mattis id, commodo id enim. Aenean sodales iaculis euismod. Donec sollicitudin molestie euismod. Pellentesque sollicitudin fermentum luctus. Nulla eget sollicitudin tortor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent malesuada et sapien vel egestas. Duis tempor suscipit dui, ac mattis quam aliquam at. Fusce quis mollis magna. Praesent tristique augue vitae finibus vehicula. Vivamus mollis vitae elit at lacinia. Nullam ac scelerisque lectus.
-    
-    Pellentesque varius aliquet eleifend. Curabitur efficitur dolor vestibulum mattis iaculis. Curabitur vestibulum ac diam quis eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras volutpat, odio varius vestibulum lobortis, ex lectus vestibulum lectus, vitae suscipit ante mauris quis nunc. Suspendisse ligula felis, imperdiet sit amet pellentesque eget, semper vitae ex. Aenean et sem at risus faucibus consectetur. Quisque leo ante, volutpat eu neque a, sodales efficitur mauris. Nunc condimentum nisl et nunc ultricies, in convallis neque consectetur. Donec sit amet metus sit amet mauris rhoncus iaculis vel facilisis magna. In lacinia ipsum lacus, sit amet mollis tortor vehicula at. Donec dignissim justo eu leo rutrum sodales. In eu mollis erat. Cras mauris dui, ultrices sodales lectus dictum, pretium feugiat magna. </div>
+  </div>
   )
 }
 
