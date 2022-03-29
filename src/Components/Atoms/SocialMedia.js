2af08e3c488253d19c9c4539c55311edc5834514
Alexander Semenov <96718145+SemenovDeveloper@users.noreply.github.com>
2022-03-29 14:49:32 +0600

    add href of social media

--- src/Components/Atoms/SocialMedia.js
+++ src/Components/Atoms/SocialMedia.js
@@ -1,23 +1,24 @@
 import React from "react";
 
 function SocialMedia({ socialNetwork }) {
-
   function rotateBorder(command) {
     const border = document.getElementById(socialNetwork.id);
     if (command === "rotate") {
       border.classList.add("rotate-social-media");
     } else {
-      border.classList.remove("rotate-social-media")
+      border.classList.remove("rotate-social-media");
     }
   }
 
   return (
-    <div className="social-media" onMouseOver={() => rotateBorder("rotate")}
-    onMouseLeave={() => rotateBorder()}>
+    <div
+      className="social-media"
+      onMouseOver={() => rotateBorder("rotate")}
+      onMouseLeave={() => rotateBorder()}
+      onClick={() => window.open(socialNetwork.href, "_blank")}
+    >
       <div id={socialNetwork.id} className="spinning-social-media"></div>
-      <i 
-        className={socialNetwork.icon}        
-      ></i>
+      <i className={socialNetwork.icon}></i>
     </div>
   );
 }
